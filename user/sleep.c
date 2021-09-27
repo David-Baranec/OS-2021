b29a7056d1179ebed2cb7ccd307343ccdf9f199e
David-Baranec <xbaranecd@stuba.sk>
2021-09-27 18:43:54 +0200

    feat: sleep not completed

---
+++ user/sleep.c
@@ -0,0 +1,13 @@
+#include "kernel/types.h"
+#include "kernel/start.h"
+#include "ussr/user.h"
+
+int
+main(int argc, char *argv[])
+{
+	int ticks;
+
+	if(argc!=2){
+		fprintf(2, "sleep usage: sleep <ticks>\n")
+
+}
