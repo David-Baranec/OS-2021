d004d82f0656fd35b93c38883e289143e17b2d8a
David-Baranec <xbaranecd@stuba.sk>
2021-10-06 10:44:08 +0200

    feature: sysinfo added, my fault I have mixed files

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -105,6 +105,7 @@ extern uint64 sys_wait(void);
 extern uint64 sys_write(void);
 extern uint64 sys_uptime(void);
 extern uint64 sys_trace(void);
+extern uint64 sys_sysinfo(void);
 
 static uint64 (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -129,14 +130,34 @@ static uint64 (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_trace]   sys_trace,
+[SYS_sysinfo] sys_sysinfo,
 };
 
 char * syscall_name[NELEM(syscalls)] = {
-  "",  "fork", "exit", "wait", "pipe",
-  "read", "kill", "exec", "fstat", "chdir",
-  "dup", "getpid", "sbrk", "sleep", "uptime",
-  "open", "write", "mknod", "unlink", "link",
-  "mkdir", "close", "trace"
+  "",  
+  "fork", 
+  "exit", 
+  "wait", 
+  "pipe",
+  "read", 
+  "kill", 
+  "exec", 
+  "fstat", 
+  "chdir",
+  "dup", 
+  "getpid", 
+  "sbrk", 
+  "sleep", 
+  "uptime",
+  "open", 
+  "write", 
+  "mknod", 
+  "unlink", 
+  "link",
+  "mkdir", 
+  "close", 
+  "trace",
+  "sysinfo",
 };
 
 void
@@ -152,7 +173,7 @@ syscall(void)
   // print trace output
   if((1 << num) & p->tracemask)
           printf("%d: syscall %s -> %d\n",p->pid,syscall_name[num],p->trapframe->a0);
-  
+
   } else {
     printf("%d %s: unknown sys call %d\n",
             p->pid, p->name, num);
